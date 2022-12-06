23a6c6f2fb20f24a93abb98ac87564c379cd735e
Fedor <fedor.kochkin48@gmail.com>
2022-12-06 04:08:11 +0300

    решение задачи 4

---
+++ zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("введите a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("введите c");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max)
+                max = b;
+if  (c > max)  
+                max = c;
+
+ Console.WriteLine(max);
