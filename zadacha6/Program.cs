f8c1ae112291ece68e8a9475359a1546269e9dcb
Fedor <fedor.kochkin48@gmail.com>
2022-12-06 04:14:04 +0300

    решение задачи 6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,6 @@
+﻿ Console.Clear();
+ Console.WriteLine("Введите число : ");
+                int a = int.Parse(Console.ReadLine());
+                if (a % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+            
