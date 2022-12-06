55b8b17f442cf7554e90ea5d01a9fd1faf6ca2b4
Fedor <fedor.kochkin48@gmail.com>
2022-12-06 04:02:20 +0300

    решение задачи 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("введите a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите b");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+                Console.WriteLine(a);
+            else 
+                Console.WriteLine(b);
+
