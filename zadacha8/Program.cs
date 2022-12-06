28ddc0b2a0c1f917816804ee8219981ba9f3f24e
Fedor <fedor.kochkin48@gmail.com>
2022-12-06 04:27:35 +0300

    решение задачи 8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0) 
+        Console.WriteLine(i); 
+       
+
+
+
+
